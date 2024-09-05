904571cc7f4ef2bf739f08bd915e4db055107270
jackson-evarts <jack.evarts@gmail.com>
2024-09-05 12:50:16 -0400

    Initial Commit

---
+++ UltimateRugbyTrainer/UltimateRugbyTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateRugbyTrainerApp.swift
+//  UltimateRugbyTrainer
+//
+//  Created by Jackson Evarts on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimateRugbyTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
