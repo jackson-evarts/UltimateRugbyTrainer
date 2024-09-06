1b008302c96a372e4e641b9ea462b93f92007eed
jackson-evarts <jack.evarts@gmail.com>
2024-09-06 15:00:47 -0400

    Fixed button and small formatting issues with ContentView.
    Working version.

--- UltimateRugbyTrainer/ContentView.swift
+++ UltimateRugbyTrainer/ContentView.swift
@@ -12,26 +12,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ZStack {
-                // Use UIColor to define a hex color
-                RoundedRectangle(cornerRadius: 5)
-                    .fill(Color(UIColor(red: 101/255, green: 198/255, blue: 236/255, alpha: 1))) // Inside color
-                    .frame(height: 100)
-                    .frame(maxWidth: .infinity)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1)), lineWidth: 10) // Outline color
-                    )
-                    .padding(.horizontal, 20)
-
+            NavigationView{
                 NavigationLink(destination: GameSimulationView()){
-                    Text("Game Simulation")
-                        .foregroundColor(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1))) // Text color
-                        .font(.title)
-                        .bold()
-                }
-            }
-        }
+                    ZStack{
+                        // Use UIColor to define a hex color
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(Color(UIColor(red: 101/255, green: 198/255, blue: 236/255, alpha: 1))) // Inside color
+                            .frame(height: 100)
+                            .frame(maxWidth: .infinity)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1)), lineWidth: 10) // Outline color
+                            )
+                            .padding(.horizontal, 20)
+                        
+                        Text("Game Simulation")
+                            .foregroundColor(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1))) // Text color
+                            .font(.title)
+                            .bold()
+                    } // Close ZStack for Game Simulation Button
+                    
+                } // Close NavigationLink
+            } // Close NavigationView
+            
+            
+            
+        } // Close outer VStack
+        
+        
+        
     }
 }
 
