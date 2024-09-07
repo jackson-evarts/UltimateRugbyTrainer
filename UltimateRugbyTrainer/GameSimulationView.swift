5170098d6feb694e96e46a4fb68e768ba74410f1
jackson-evarts <jack.evarts@gmail.com>
2024-09-07 14:58:54 -0400

    This version contains a whole stopwatch functionality from this tutorial: https://www.youtube.com/watch?v=ZFN5PQ4SPuQ I think there is definitely some useful stuff within this code that will be useful in the future, specifically when we start working with time, it will be useful to have the stopwatch stuff for running the clock for when each audiobyte should play.

--- UltimateRugbyTrainer/GameSimulationView.swift
+++ UltimateRugbyTrainer/GameSimulationView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct GameSimulationView: View {
+    
+    @ObservedObject var gameSimulationManager = GameSimulationViewManager()
+    
     var body: some View {
         VStack {
-            Text("Welcome to the Game Simulation Menu Options")
+            
+            // This is the displayed text for the timer. TODO: Eventually may want to have a countdown to halftime instead of simply counting up.
+            Text(String(format: "%.1f", gameSimulationManager.secondsElapsed))
                 .font(.largeTitle)
-                .padding()
+                .padding(.top, 200)
+                .padding(.bottom,100)
+                .padding(.trailing,100)
+                .padding(.leading,100)
+            
+            // The different buttons for the stopwatch view depending on the current state of the stopwatch. Ex: When it's stopped you can press start.
+            if gameSimulationManager.timerState == .stopped {
+                Button(action: {self.gameSimulationManager.start()}){
+                    TimerButton(label: "Start", buttonColor: .yellow, textColor: .black)
+                }
+            }
+                
+            if gameSimulationManager.timerState == .running{
+                Button(action: {self.gameSimulationManager.pause()}){
+                    TimerButton(label: "Pause", buttonColor: .yellow, textColor: .black)
+                }
+            }
+            
+            // When the stopwatch is paused, you can press start or stop, so there are two buttons
+            if gameSimulationManager.timerState == .paused{
+                Button(action: {self.gameSimulationManager.start()}){
+                    TimerButton(label: "Start", buttonColor: .yellow, textColor: .black)
+                }
+                Button(action: {self.gameSimulationManager.stop()}){
+                    TimerButton(label: "Stop", buttonColor: .red, textColor: .white)
+                }
+                .padding(.top, 30)
+                
+            }
+            Spacer()
         }
         .navigationTitle("Game Simulation")
     }
@@ -20,4 +54,21 @@ struct GameSimulationView: View {
 
 #Preview {
     GameSimulationView()
+}
+
+struct TimerButton: View { // This allows for multiple instances of a TimerButton, so we can have 2 when the button is paused or a different color button for the Stop state.
+    
+    let label: String
+    let buttonColor: Color
+    let textColor: Color
+    
+    
+    var body: some View {
+        Text(label)
+            .foregroundColor(textColor)
+            .padding(.vertical, 20)
+            .padding(.horizontal, 90)
+            .background(buttonColor)
+            .cornerRadius(10)
+    }
 }
---
+++ UltimateRugbyTrainer/GameSimulationViewManager.swift
@@ -0,0 +1,45 @@
+//
+//  GameSimulationViewManager.swift
+//  UltimateRugbyTrainer
+//
+//  Created by Jackson Evarts on 9/7/24.
+//
+
+import Foundation
+import SwiftUI
+
+// Must be an ObservableObject so that the derived values from this class can update the GameSimulationView
+class GameSimulationViewManager: ObservableObject {
+    
+    enum timerMode{
+        case running
+        case stopped
+        case paused
+        
+    }
+    @Published var timerState: timerMode = .stopped
+    
+    // The "@Published" property wrapper tells all observing views to reload themselves whenver secondsElapsed changes
+    @Published var secondsElapsed = 0.0
+    var timer = Timer()
+    func start(){
+        timerState = .running
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){
+            timer in
+            self.secondsElapsed += 0.1
+        }
+    }
+    
+    func pause(){
+        timer.invalidate()
+        timerState = .paused
+    }
+    
+    func stop(){
+        timer.invalidate()
+        secondsElapsed = 0
+        timerState = .stopped
+    }
+    
+    
+}
