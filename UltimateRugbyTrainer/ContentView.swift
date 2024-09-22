42e0d67e58504febafee665e05e79beb1c23d2ce
jackson-evarts <jack.evarts@gmail.com>
2024-09-22 15:57:10 -0400

    Starting the process of converting all the navigation views into a navigation path that works as a stack for more dynamic control over the UI navigation. Only one button so far and still learning about how it works so I basically have to deintegrate everything.

--- UltimateRugbyTrainer/ContentView.swift
+++ UltimateRugbyTrainer/ContentView.swift
@@ -10,39 +10,46 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State private var path = NavigationPath()
+    
     var body: some View {
-        VStack {
-            NavigationView{ // Game Simulation Button
-                NavigationLink(destination: GameSimulationView()){
-                    ZStack{
-                        // Use UIColor to define a hex color
-                        RoundedRectangle(cornerRadius: 5)
-                            .fill(Color(UIColor(red: 101/255, green: 198/255, blue: 236/255, alpha: 1))) // Inside color
-                            .frame(height: 100)
-                            .frame(maxWidth: .infinity)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 5)
-                                    .stroke(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1)), lineWidth: 10) // Outline color
-                            )
-                            .padding(.horizontal, 20)
-                        
-                        Text("Game Simulation")
-                            .foregroundColor(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1))) // Text color
-                            .font(.custom("NFPixels-Regular", size: 40))
-                            .bold()
-                            .scaledToFit()
-                        
-                    } // Close ZStack for Game Simulation Button
+        NavigationStack(path: $path) {
+            VStack{ // Game Simulation Button
+                
+                
+                Button("Game Simulation"){
+                    path.append(GameSimulationView())
                     
-                } // Close NavigationLink
-            } // Close NavigationView (the Game Simulation Button)
-            
-            
-            
+                }
+                
+                /*
+                 ZStack{
+                 // Use UIColor to define a hex color
+                 RoundedRectangle(cornerRadius: 5)
+                 .fill(Color(UIColor(red: 101/255, green: 198/255, blue: 236/255, alpha: 1))) // Inside color
+                 .frame(height: 100)
+                 .frame(maxWidth: .infinity)
+                 .overlay(
+                 RoundedRectangle(cornerRadius: 5)
+                 .stroke(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1)), lineWidth: 10) // Outline color
+                 )
+                 .padding(.horizontal, 20)
+                 
+                 Text("Game Simulation")
+                 .foregroundColor(Color(UIColor(red: 5/255, green: 117/255, blue: 162/255, alpha: 1))) // Text color
+                 .font(.custom("NFPixels-Regular", size: 40))
+                 .bold()
+                 .scaledToFit()
+                 
+                 } // Close ZStack for Game Simulation Button
+                 
+                 } // Close NavigationView (the Game Simulation Button)
+                 
+                 */
+            }
         } // Close outer VStack
-        
-        
-        
+    
     }
 }
 
--- UltimateRugbyTrainer/GameSimulationView.swift
+++ UltimateRugbyTrainer/GameSimulationView.swift
@@ -7,13 +7,13 @@
 
 import SwiftUI
 
-struct GameSimulationView: View{
+struct GameSimulationView: View, Hashable{
     var body: some View {
         VStack{
             // TODO: Secondary menu title (7s Simulation)
             // here
             // TODO: Tutorial button nav to Tutorial Page
-            
+            Text("You are on the GameSimulationView")
             // TODO: Intensity slider
             
             // TODO: Play button nav to gameplay screen
