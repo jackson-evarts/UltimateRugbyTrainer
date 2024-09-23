497ca8502836ba541756c0f3002b2801013acab8
jackson-evarts <jack.evarts@gmail.com>
2024-09-22 20:16:49 -0400

    Finished version of converting the app into a navigation stack. I lost all the graphics I had before but that's something to focus on after the practicalities of the app are handled

---
+++ TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  UltimateRugbyTrainer
+//
+//  Created by Jackson Evarts on 9/22/24.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
