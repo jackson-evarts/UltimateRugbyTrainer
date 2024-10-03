1f03c42ea713c09af533a93d62a76ec1c19d21d6
jackson-evarts <jack.evarts@gmail.com>
2024-10-03 13:27:53 -0400

    Colors feature beginnings if Mikas and Joe want to take over on the color aspects of the app. If not we will leave those 2 options. Maybe adding an Australia option??

--- UltimateRugbyTrainer/ContentView.swift
+++ UltimateRugbyTrainer/ContentView.swift
@@ -12,14 +12,14 @@ struct ContentView: View {
     @ObservedObject var colorSchemeModel: ColorSchemeModel
     /*
      TO USE CUSTOM COLORS:
-     colorSchemeModel.colorScheme.[D1, D2, D3, mainDetail, or accent]
+     colorSchemeModel.colorScheme.[D1, D2, D3, D4, or D5]
      */
     
     var body: some View {
         NavigationStack {
             ZStack {
                 // Background color filling the entire screen
-                colorSchemeModel.colorScheme.D2 // Background color #8585a4
+                colorSchemeModel.colorScheme.D2 // Background color
                     .edgesIgnoringSafeArea(.all) // Fill entire background
                 
                 VStack {
@@ -39,11 +39,11 @@ struct ContentView: View {
                             .font(.custom("NFPixels-Regular", size: 30))
                             .padding()
                             .background(colorSchemeModel.colorScheme.D4) // Background color #ccccd9
-                            .foregroundColor(colorSchemeModel.colorScheme.D3) // Text color #3c3c6c
+                            .foregroundColor(colorSchemeModel.colorScheme.D1) // Text color #3c3c6c
                             .cornerRadius(10)
                             .overlay(
                                 RoundedRectangle(cornerRadius: 10)
-                                    .stroke(colorSchemeModel.colorScheme.D5, lineWidth: 5) // Outline (border) color #3c3c6c
+                                    .stroke(colorSchemeModel.colorScheme.D3, lineWidth: 5) // Outline (border) color #3c3c6c
                             )
                     }
                     
@@ -52,39 +52,37 @@ struct ContentView: View {
                         Text("Game Simulation")
                             .font(.custom("NFPixels-Regular", size: 30))
                             .padding()
-                            .background(colorSchemeModel.colorScheme.D4) // Background color #ccccd9
-                            .foregroundColor(colorSchemeModel.colorScheme.D3) // Text color #3c3c6c
+                            .background(colorSchemeModel.colorScheme.D4) // Background color
+                            .foregroundColor(colorSchemeModel.colorScheme.D1) // Text color
                             .cornerRadius(10)
                             .overlay(
                                 RoundedRectangle(cornerRadius: 10)
-                                    .stroke(colorSchemeModel.colorScheme.D5, lineWidth: 5) // Outline (border) color #3c3c6c
+                                    .stroke(colorSchemeModel.colorScheme.D3, lineWidth: 5) // Outline (border) color
                             )
                     }
                     
                     // Bronco Trainer Button (disabled)
-                    Button("Bronco Trainer") {
-                        // TODO: Add functionality here
-                    }
-                    .font(.custom("NFPixels-Regular", size: 30))
-                    .padding()
-                    .background(Color.gray) // Background color #ccccd9
-                    .foregroundColor(colorSchemeModel.colorScheme.D3) // Text color #3c3c6c
-                    .cornerRadius(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(colorSchemeModel.colorScheme.D5, lineWidth: 5) // Outline (border) color #3c3c6c
-                    )
+                    Text("Bronco Trainer")
+                        .font(.custom("NFPixels-Regular", size: 30))
+                        .padding()
+                        .background(Color.gray) // Background color #ccccd9
+                        .foregroundColor(Color.white) // Text color #3c3c6c
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.white, lineWidth: 5) // Outline (border) color #3c3c6c
+                        )
                     
                     Text("Coming soon!")
                         .font(.custom("NFPixels-Regular", size: 20))
-                        .foregroundColor(colorSchemeModel.colorScheme.D3)
+                        .foregroundColor(colorSchemeModel.colorScheme.D1)
                     Spacer()
                 }
                 .padding() // Add padding to avoid content getting too close to the edges
             }
             .overlay(
                 RoundedRectangle(cornerRadius: 300) // Set cornerRadius to 0 to avoid rounding the frame
-                    .stroke(colorSchemeModel.colorScheme.D3, lineWidth: 20) // Frame color #b42434
+                    .stroke(colorSchemeModel.colorScheme.D4, lineWidth: 20) // Frame color #b42434
                     .clipShape(RoundedRectangle(cornerRadius: 30)) // Clip to the shape to ensure the corners are matched
                 
                     .edgesIgnoringSafeArea(.all) // Ensure frame fills the entire screen
