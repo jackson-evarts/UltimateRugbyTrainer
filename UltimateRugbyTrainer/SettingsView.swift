292a62cbb3177f0dee827eca6a6159db8d26f10a
jackson-evarts <jack.evarts@gmail.com>
2024-10-03 13:00:41 -0400

    Custom Colors (UGLY VERSION)

---
+++ UltimateRugbyTrainer/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  UltimateRugbyTrainer
+//
+//  Created by Jackson Evarts on 10/3/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
