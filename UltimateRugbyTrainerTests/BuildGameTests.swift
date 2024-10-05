c08eb9806252d4e44c4ec7e7d237a508d2b238cf
jackson-evarts <jack.evarts@gmail.com>
2024-10-05 10:13:45 -0400

    experimenting with test

---
+++ UltimateRugbyTrainerTests/BuildGameTests.swift
@@ -0,0 +1,29 @@
+//
+//  BuildGameTests.swift
+//  UltimateRugbyTrainerTests
+//
+//  Created by Jackson Evarts on 10/5/24.
+//
+
+import XCTest
+@testable import UltimateRugbyTrainer
+
+final class BuildGameTests: XCTestCase {
+
+    // 1. Happy Path
+    func testSuccessfulIntensity1Build(){
+        // a. Given (Arrange)
+        let intensity = 1
+        let playLogic = PlayLogic()
+        
+        
+        // b. When (Act)
+        let gameArr = playLogic.buildGame(intensity: intensity)
+        
+        
+        // c. Then (Assert)
+        // XCTAssert(gameArr.contains(where: 0))
+        
+    }
+
+}
